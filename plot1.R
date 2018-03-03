c7320b6781992bd6db6a05b33128f6e26eb23390
rochevil <32667453+rochevil@users.noreply.github.com>
2018-03-03 18:58:20 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+## Downloads the file online if it does not exist in the working directory
+
 datafilespath <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 datafile <- "household_power_consumption.txt"
 
@@ -6,8 +8,12 @@ if (!file.exists(datafile))
         download.file(datafilespath, datafileszip)
 }
 
+## Subsets the file to only get entries between 1/2/2007 and 2/2/2007
+
 data <- read.table(datafile,header=TRUE, sep=";", na.strings="?")
 data_needed <- subset(data,Date %in% c("1/2/2007","2/2/2007"))
+
+## Creates a histogram named Global Active Power and saves the plot to a PNG file
 
 png("plot1.png", width=480, height=480, bg = "transparent")
 hist(data_needed[,3],col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
